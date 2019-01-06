e83b4e91684b2bd91f496e43467780684e5bf35f
Metehan Goksel Kurtulan <44157440+mkurtt@users.noreply.github.com>
2019-01-06 11:45:24 +0300

    Add files via upload

---
+++ C#Examples/Lectures/AbstractClass/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.ExampleMethod();
+
+        }
+    }
+
+    abstract class Person
+    {
+        public abstract void RoamInCorridor();
+
+        public string ExampleMethod()
+        {
+            return "Value";
+        }
+    }
+
+    class Student : Person
+    {
+        public override void RoamInCorridor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Teacher : Person
+    {
+        public override void RoamInCorridor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Labor : Person
+    {
+        public override void RoamInCorridor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
